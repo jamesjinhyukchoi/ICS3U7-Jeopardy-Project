264af15938d766da883a88789a398e1478a8bbe8
Chen-E <evanchen02@gmail.com>
2018-05-31 11:15:26 -0400

    Add files via upload

--- CategoryGameData.java
+++ CategoryGameData.java
@@ -4,7 +4,7 @@ import java.io.FileReader;
 import java.io.FileNotFoundException;
 
 /**
- * Reads a data file and processes the categroy game data suitable for class JeopardyGame.
+ * Reads a data file and processes the category game data suitable for class JeopardyGame.
  * 
  * @author Evan Chen & James Choi
  * @version 2018-05-30
@@ -22,36 +22,49 @@ public class CategoryGameData
     /**
      * Creates an object of class CategoryGameData containing the category game data from the specified file.
      * 
-     * @param fileName the name of the file that is being read from; must be a category file
+     * @param fileName the specified file; must be in the category format,
+     * example can be found @ http://touque.ca/EC/programming/Java/assignments/ca/Jeopardy/dataFiles/categories.data
      */
     public CategoryGameData(String fileName)
     {
+        // Initialize the array.
+        category = new String[NUMBER_OF_CATEGORIES];
+
         // Attempt to establish a connection to the data file.
-        if (fileName != null)
+        if (fileName == null)
+        {
+            for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES; categoryNumber++)
+            {
+                category[categoryNumber] = "";
+            } // end of for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES; categoryNumber++)
+        }
+        else
         {
             try
             {
                 fileReader = new BufferedReader(new FileReader(fileName));
+                
+                // Load the data into the array.
+                for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES; categoryNumber++)
+                {
+                    try
+                    {
+                        category[categoryNumber] = fileReader.readLine();
+                    }
+                    catch (IOException exception)
+                    {
+                        // Account for errors.
+                        category[categoryNumber] = "";
+                    } // end of catch (IOException exception)
+                } // end of for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES = 5; categoryNumber++)
             }
             catch (FileNotFoundException exception)
             {
-                return;
+                for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES; categoryNumber++)
+                {
+                    category[categoryNumber] = "";
+                } // end of for (int categoryNumber = 0; categoryNumber < NUMBER_OF_CATEGORIES; categoryNumber++)
             } // catch (FileNotFoundException exception)
-            for (int index = 0; index < NUMBER_OF_CATEGORIES; index++)
-            {
-                try
-                {
-                    category[index] = fileReader.readLine();
-                }
-                catch (IOException exception)
-                {
-                    category[index] = "";
-                } // end of catch (IOException exception)
-            } // end of for (int index = 0; index < NUMBER_OF_CATEGORIES = 5; index++)
-            for (int index = 0; index < NUMBER_OF_CATEGORIES; index++)
-            {
-                category[index] = "";
-            } // end of for (int index = 0; index < NUMBER_OF_CATEGORIES; index++)
         } // end of catch (FileNotFoundException exception)
 
     } // end of constructor CategoryGameData()
